ed0300ff073deb2d675c96e6f15ec6ef05eafbee
AlberteDiaz <9003857@alumnos.ufv.es>
2024-01-26 22:29:42 +0100

    uwu6

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    public Image ZImage, XImage, CImage, VImage, HImage, JImage, KImage, LImage;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,47 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void useAbility(int id)
+    {
+        if (id == 0)
+        {
+            
+        }
+        else if (id == 1)
+        {
+
+        }
+        else if (id == 2)
+        {
+
+        }
+        else if (id == 3)
+        {
+
+        }
+        else if (id == 4)
+        {
+
+        }
+        else if (id == 5)
+        {
+
+        }
+        else if (id == 6)
+        {
+
+        }
+        else if (id == 7)
+        {
+
+        }
+        else if (id == 8)
+        {
+
+        }
+
+    }
+
+    
 }
